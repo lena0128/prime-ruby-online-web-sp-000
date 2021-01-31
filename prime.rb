fa1d11e4bed3c1eb3acbc056d2cb7988a0ca4899
Lena Hou <lenahou2013@gmail.com>
2021-01-31 17:46:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,10 +5,11 @@ def prime?(num)
   if num <= 1
     return false
   else num > 1
-  i = 2
-  while i < num
-    return false if num % i == 0
-    i += 1
-end
-   true
+    i = 2
+    while i < num
+      return false if num % i == 0
+      i += 1
+    end
+    true
+    end
 end
