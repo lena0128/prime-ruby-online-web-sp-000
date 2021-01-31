6e79cce59d41f2d9a419d91190da941ef13da3ff
Lena Hou <lenahou2013@gmail.com>
2021-01-31 17:30:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 
 def prime?(integer)
-  if integer % integer == 0
-    return true
-
+  i = 2
+  while i < integer
+  if integer % 2 == 0
+    return false
+    i = i + 1
+  end
+  return true
 end
