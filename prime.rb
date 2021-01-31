374c64c42c4fd3bbdd239c245a45ff5ea58467f4
Lena Hou <lenahou2013@gmail.com>
2021-01-31 17:52:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(num)
+  #Using a while loop
+  if num <= 1
+    return false
+  else num > 1
+    i = 2
+    while i < num
+      return false if num % i == 0
+      i += 1
+    end
+    true
+    end
+end
